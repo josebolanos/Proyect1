01bc6de6d5d559d902376d7b87bace699856f458
Your Name <you@example.com>
2020-12-01 20:56:07 +0000

    Nuevo Proyecto

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// Variables
+
+
+eventListener();
+function eventListener() {
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+}
+eventListener();
+function eventListener() {
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+}
+
+
+
+
+
+
+
+
+// Funciones
+
+function iniciarApp() {
+    console.log('Inicilizando');
+}
+
+
+
+
+
+
