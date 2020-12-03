9e15c4ce34a2f24802f60b0a59864539bcdc4070
Jose <josebolanoslanzarote@gmail.com>
2020-12-03 18:11:18 +0000

    Proyecto parte5

--- js/app.js
+++ js/app.js
@@ -41,18 +41,39 @@ function iniciarApp() {
 // Valida el formulario
 function validarFormulario(e) {
 
+  
     if(e.target.value.length > 0) {
-        console.log('Si hay algo');
+          // Elimina los errores
+        const error = document.      querySelector('p.error');
+        error.remove();
+
+
+        e.target.classList.remove('border', 'border-green-500');
+        e.target.classList.add('border', 'border-green-500');
     } else {
+        e.target.classList.remove('border', 'border-green-500');
         e.target.classList.add('border', 'border-red-500');
 
         mostrarError('Todos los campos son obligatorios');
     }
 
     if(e.target.type === 'email') {
-        const resultado = e.target.value.indexOf('@');
-        if(resultado < 0) {
-            mostrarError('El email no es valido');
+        const er =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      
+        if( er.test( e.target.value)) {
+
+            const error = document.      querySelector('p.error');
+            error.remove();
+    
+    
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-green-500');
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+    
+
+            mostrarError('Email no es valido');
         }
     }
 }
